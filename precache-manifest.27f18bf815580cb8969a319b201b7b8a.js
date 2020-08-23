37c5fec86f8c957277704ea37351242fcd966538
David Manye <david.manye@smadex.com>
2020-08-23 12:57:55 +0200

    Updates

---
+++ precache-manifest.27f18bf815580cb8969a319b201b7b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0714acd6b0e97cbdf3721c8ade200e",
+    "url": "/xanda/index.html"
+  },
+  {
+    "revision": "b0f5c0f173212b88908a",
+    "url": "/xanda/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59809ceabc492ce891b8",
+    "url": "/xanda/static/js/2.b4224cae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xanda/static/js/2.b4224cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f5c0f173212b88908a",
+    "url": "/xanda/static/js/main.18a19c9f.chunk.js"
+  },
+  {
+    "revision": "ae61dd76a29264cc1073",
+    "url": "/xanda/static/js/runtime-main.05a6976a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xanda/static/media/logo.5d5d9eef.svg"
+  }
+]);
