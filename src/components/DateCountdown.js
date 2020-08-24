e35e551ee0ffc9053509677701e9b1bd8812e396
David Manye <david.manye@smadex.com>
2020-08-24 13:21:58 +0200

    Initial commit

---
+++ src/components/DateCountdown.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {CircularProgressbar} from 'react-circular-progressbar';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import 'react-circular-progressbar/dist/styles.css';
+import moment from "moment";
+
+class DateCountdown extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      final: new moment('2020-09-13T18:00:00'),
+      date: new moment()
+    };
+  }
+
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new moment()
+    });
+  }
+
+  render() {
+    const duration = moment.duration(this.state.final.diff(this.state.date))
+    const day = duration.days();
+    const hour = duration.hours();
+    const minute = duration.minutes();
+    const seconds = duration.seconds();
+    return <Container>
+      <Row xs={4} md={8} lg={16}>
+        <Col>
+          <h2>Dias</h2>
+          <CircularProgressbar value={day} maxValue={30} text={`${day}`}/>
+        </Col>
+        <Col>
+          <h2>Horas</h2>
+          <CircularProgressbar value={hour} maxValue={24} text={`${hour}`}/>
+        </Col>
+        <Col>
+          <h2>Minutos</h2>
+          <CircularProgressbar value={minute} maxValue={60} text={`${minute}`}/>
+        </Col>
+        <Col>
+          <h2>Segundos</h2>
+          <CircularProgressbar value={seconds} maxValue={60} text={`${seconds}`}/>
+        </Col>
+      </Row>
+    </Container>
+  }
+}
+
+export default DateCountdown;
